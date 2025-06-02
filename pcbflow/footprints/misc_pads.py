296d056053ea80827b080c397106a89b10af95b8
hkwarner <hwarner@g.harvard.edu>
2025-06-02 14:34:32 -0400

    ask masks

--- pcbflow/footprints/misc_pads.py
+++ pcbflow/footprints/misc_pads.py
@@ -14,6 +14,15 @@ class Discrete2(PCBPart):
             self.pads[1].set_name(pad2)
         return self
 
+class Discrete_mask(PCBPart):
+    def assign_pads(self, pad1=None, pad2=None):
+        layer = 'GTO'
+        if pad1 is not None:
+            self.pads[0].set_name(pad1)
+        if pad2 is not None:
+            self.pads[1].set_name(pad2)
+        return self
+
 class wb_pad_12(Discrete2):
     def __init__(self, *args, family="C", **kwargs):
         self.family = family
@@ -105,6 +114,122 @@ class rf_pad_y(Discrete2):
 
         # self.label(dc)
         # dc.pop()
+
+    def escape_2layer(self):
+        # escape for 2-layer board (VCC on GTL, GND on GBL)
+        self.pads[0].set_name("VCC").w("o f 0.5").wire()
+        self.pads[1].w("o -")
+
+    def smp(Discrete2):
+        def __init__(self, *args, family="C", **kwargs):
+            self.family = family
+            super().__init__(*args, **kwargs)
+
+        def place(self, dc):
+            w = MILS(38)
+            h = 1.5*MILS(50)
+            
+            dc.rect(w, h)
+            self.smd_pad(dc, ignore_paste = False)
+            self.id = [0]
+
+class smp_ko(Discrete_mask):
+    def __init__(self, *args, family="C", **kwargs):
+        self.family = family
+        super().__init__(*args, **kwargs)
+
+    def place(self, dc):
+        trace_width = MILS(38)
+        w= 3.97-trace_width/2
+        h = 2.73-trace_width/2
+        
+        dc.rect(w, h)
+        self.smd_pad(dc, ignore_paste = False)
+        self.id = [0]
+
+        # dc.silk(side=self.side)
+
+        # self.label(dc)
+        # dc.pop()
+
+    def escape_2layer(self):
+        # escape for 2-layer board (VCC on GTL, GND on GBL)
+        self.pads[0].set_name("VCC").w("o f 0.5").wire()
+        self.pads[1].w("o -")
+
+class omnetics(Discrete2):
+    def __init__(self, *args, family="C", **kwargs):
+        self.family = family
+        super().__init__(*args, **kwargs)
+
+    def place(self, dc):
+        # Pads on either side
+        h = .380 
+        w = 1.08
+        n = 10
+        pitch = .640
+        
+        for idx in range(n):
+            if idx > 0:
+                dc.forward(pitch)
+            dc.rect(w,h)
+            self.smd_pad(dc)
+            dc.rect(w+.01, h+.01)
+            # dc.silk(side=self.side)   
+            self.id = [idx]
+
+    def escape_2layer(self):
+        # escape for 2-layer board (VCC on GTL, GND on GBL)
+        self.pads[0].set_name("VCC").w("o f 0.5").wire()
+        self.pads[1].w("o -")
+
+class wb_4(Discrete2):
+    def __init__(self, *args, family="C", **kwargs):
+        self.family = family
+        super().__init__(*args, **kwargs)
+
+    def place(self, dc):
+        # Pads on either side
+        w = .7
+        h = .3
+        n = 4
+        pitch = .4
+        dc.w('r 90')
+        for idx in range(n):
+
+            if idx > 0:
+                dc.forward(pitch)
+            dc.rect(w,h)
+            self.smd_pad(dc)
+            dc.rect(w+.01, h+.01)
+            # dc.silk(side=self.side)   
+            self.id = [idx]
+
+    def escape_2layer(self):
+        # escape for 2-layer board (VCC on GTL, GND on GBL)
+        self.pads[0].set_name("VCC").w("o f 0.5").wire()
+        self.pads[1].w("o -")
+
+class wb_14(Discrete2):
+    def __init__(self, *args, family="C", **kwargs):
+        self.family = family
+        super().__init__(*args, **kwargs)
+
+    def place(self, dc):
+        # Pads on either side
+        h = .300 
+        w = .700
+        n = 14
+        pitch = .40
+        
+        for idx in range(n):
+            if idx > 0:
+                dc.forward(pitch)
+            dc.rect(w,h)
+            self.smd_pad(dc)
+            dc.rect(w+.01, h+.01)
+            # dc.silk(side=self.side)   
+            self.id = [idx]
 
     def escape_2layer(self):
         # escape for 2-layer board (VCC on GTL, GND on GBL)
